28bdaa046f20a36212a105b9beeb8eabeb519a62
2211轩岚诺 <23096442d@connect.polyu.hk>
2025-12-08 23:56:01 +0800

    Update Matlab node activation Code.m

--- task1/Matlab node activation Code.m
+++ task1/Matlab node activation Code.m
@@ -1,5 +1,3 @@
 setenv('ROS_MASTER_URI','http://192.168.0.100:11311')
 setenv('ROS_IP','()') % IP value is subjected to change base on the user's server IP address
-
 rosinit('NodeName','/matnode')
-
