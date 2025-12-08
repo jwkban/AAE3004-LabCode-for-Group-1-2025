bed3d0a6ed85a4d36a8e5e12fee835429d3756ca
2211轩岚诺 <23096442d@connect.polyu.hk>
2025-12-08 23:54:15 +0800

    Update and rename wed.m to task1/Matlab Node activation Code.m

---
+++ task1/Matlab Node activation Code.m
@@ -0,0 +1,4 @@
+setenv('ROS_MASTER_URI','http://192.168.0.100:11311')
+setenv('ROS_IP','192.168.0.149') %
+
+rosinit('NodeName','/matnode')
--- wed.m
+++
@@ -1,3 +0,0 @@
-setenv('ROS_MASTER_URI','http://192.168.0.100:11311')
-setenv('ROS_IP','192.168.0.149')
-rosinit('NodeName','/matnode')
